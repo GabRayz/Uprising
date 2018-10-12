cd980d57580617101c9f3335bc747ba7b5363230
Gabriel Rayzal <gabriel.rayzal@gmail.com>
2018-10-12 20:25:38 +0200

    Added the mirror, but bugged : The view angle isnt working

---
+++ ProjectS2/Assets/mirror.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mirror : MonoBehaviour {
+
+    public Transform PlayerCam;
+    public Transform MirrorCam;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void CalculRotation() {
+        Vector3 direction = (PlayerCam.position - transform.position).normalized;
+        Quaternion rotation = Quaternion.LookRotation(direction);
+
+        rotation.eulerAngles = transform.eulerAngles - rotation.eulerAngles;
+        MirrorCam.localRotation = rotation;
+    }
+}
