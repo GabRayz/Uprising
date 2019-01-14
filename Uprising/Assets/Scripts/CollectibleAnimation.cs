689ad40353a2288a85a5d0e7a5e8eeaea822193d
Gabriel Rayzal <gabriel.rayzal@gmail.com>
2019-01-14 20:44:59 +0100

    Added predab for collectible objects

---
+++ Uprising/Assets/Scripts/CollectibleAnimation.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollectibleAnimation : MonoBehaviour {
+
+    public Rigidbody collectible;
+    private Vector3 rotation;
+
+	// Use this for initialization
+	void Start () {
+        collectible = GetComponent<Rigidbody>();
+        rotation = new Vector3(60, 60, 60);
+	}
+	
+    private void FixedUpdate()
+    {
+        Quaternion deltaRot = Quaternion.Euler(rotation * Time.deltaTime);
+        collectible.MoveRotation(collectible.rotation * deltaRot);
+    }
+}
