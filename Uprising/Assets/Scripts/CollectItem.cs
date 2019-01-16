62bfff4035638ffbfaeddce611b7f0015ebc0819
Gabriel Rayzal <gabriel.rayzal@gmail.com>
2019-01-16 20:25:29 +0100

    Items triggers collision events. Added a testing script to test collisions

---
+++ Uprising/Assets/Scripts/CollectItem.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollectItem : MonoBehaviour {
+    // This script is to be attached to the player. It tests the collision with a item.
+    public Rigidbody player;
+
+	// Use this for initialization
+	void Start () {
+        player = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.CompareTag("item"))
+        {
+            other.gameObject.SetActive(false);
+        }
+    }
+}
