3174981422cd366541071f207900f805bc715ebe
Gabriel Rayzal <gabriel.rayzal@gmail.com>
2019-01-21 14:17:32 +0100

    Added itemController script

---
+++ Uprising/Assets/Scripts/ItemController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Item controller.
+/// Declares classes for collected item, currently in tha player's inventory.
+/// </summary>
+
+public class ItemController : MonoBehaviour
+{
+    private class Item
+    {
+        private int durability;
+
+        public int GetDurability()
+        {
+            return durability;
+        }
+    }
+
+    private interface IBonus
+    {
+
+    }
+}
