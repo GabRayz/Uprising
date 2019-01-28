3c61ab9be49c9bc22c9107d5f9677ca7d089be7d
Vardiak <contact@vardiak.fr>
2019-01-28 16:03:59 +0100

    NotworkTest

---
+++ NotworkTest/Assets/NotworkManager.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class NotworkManager : MonoBehaviourPunCallbacks
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Connect();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void Awake()
+    {
+        PhotonNetwork.AutomaticallySyncScene = true;
+    }
+
+    void Connect()
+    {
+        if (PhotonNetwork.IsConnected)
+        {
+            Debug.Log("Join random room");
+            PhotonNetwork.JoinRandomRoom();
+        }
+        else
+        {
+            Debug.Log("Connect to server");
+            PhotonNetwork.GameVersion = "0.1";
+            PhotonNetwork.ConnectUsingSettings();
+        }
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        Debug.Log("OnConnectedToMaster() was called by PUN.");
+        PhotonNetwork.JoinRandomRoom();
+    }
+
+    public override void OnJoinRandomFailed(short returnCode, string message)
+    {
+        Debug.Log("PUN Basics Tutorial/Launcher:OnJoinRandomFailed() was called by PUN. No random room available, so we create one.\nCalling: PhotonNetwork.CreateRoom");
+
+        // #Critical: we failed to join a random room, maybe none exists or they are all full. No worries, we create a new room.
+        PhotonNetwork.CreateRoom("MyNotRoom");
+    }
+
+    public override void OnJoinedRoom()
+    {
+        PhotonNetwork.Instantiate("PlayerPrefab", new Vector3(0, 0, 0), Quaternion.identity, 0);
+    }
+}
---
+++ NotworkTest/Assets/PlayerController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class PlayerController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (this.GetComponent<PhotonView>().IsMine)
+        {
+            if (Input.GetKey(KeyCode.Z))
+                transform.position += transform.forward * Time.deltaTime * 5;
+            if (Input.GetKey(KeyCode.Q))
+                transform.position -= transform.right * Time.deltaTime * 5;
+            if (Input.GetKey(KeyCode.S))
+                transform.position -= transform.forward * Time.deltaTime * 5;
+            if (Input.GetKey(KeyCode.D))
+                transform.position += transform.right * Time.deltaTime * 5;
+        }
+    }
+}
