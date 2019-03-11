3aa6456a7e0396beaef0391136b61fcc92ae3298
Tony Heng <tony.heng@epita.fr>
2019-03-11 16:05:32 +0100

    tony

--- Uprising/Assets/Scripts/Weapons Scripts/DefaultGun.cs
+++ Uprising/Assets/Scripts/Weapons Scripts/DefaultGun.cs
@@ -22,19 +22,17 @@ namespace Uprising.Items
 
         public override void Use()
         {
-            Belette NewBelette = new Belette(range, belette, this);
+            //Belette NewBelette = new Belette(range, belette, this);
+            //GameObject NewBelette = Instantiate(belette, weapon.gameObject.transform);
+
+            //NewBelette.GetComponent<Rigidbody>().AddForce(weapon.gameObject.transform.forward * 100);
+
             //GameObject NewBelette = Instantiate(belette, this.transform);
             //NewBelette.GetComponent<Rigidbody>().AddForce(this.transform.forward * 100);
-        }
 
-        public override void Select()
-        {
-            weapon.SetActive(true);
-        }
+            player.transform.Find("h_defaultgun").GetComponent<belettegen>().shoot(range, durability);
+            Debug.Log("message");
 
-        public override void Unselect()
-        {
-            weapon.SetActive(false);
         }
     }
 }
---
+++ Uprising/Assets/belettegen.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class belettegen : MonoBehaviour
+{
+    public GameObject belette;
+    public void shoot(float range, int durability)
+    {
+        GameObject NewBelette = Instantiate(belette, this.gameObject.transform);
+        NewBelette.GetComponent<Rigidbody>().AddForce(this.gameObject.transform.forward * 100);
+    }
+}
