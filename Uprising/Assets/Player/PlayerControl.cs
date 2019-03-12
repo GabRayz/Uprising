c732ce7f7f8f08472bbe1910cf6e002e981e2f87
Gabriel Rayzal <gabriel.rayzal@gmail.com>
2019-03-12 19:14:00 +0100

    Increase CheckgroundStatus distance

--- Uprising/Assets/Player/PlayerControl.cs
+++ Uprising/Assets/Player/PlayerControl.cs
@@ -273,11 +273,11 @@ namespace Uprising.Players
             RaycastHit hitInfo;
 #if UNITY_EDITOR
             // helper to visualise the ground check ray in the scene view
-            Debug.DrawLine(transform.position + (Vector3.up * 0.1f), transform.position + (Vector3.up * 0.1f) + (Vector3.down * 0.1f));
+            Debug.DrawLine(transform.position + (Vector3.up * 0.1f), transform.position + (Vector3.up * 0.1f) + (Vector3.down * 0.15f));
 #endif
             // 0.1f is a small offset to start the ray from inside the character
             // it is also good to note that the transform position in the sample assets is at the base of the character
-            if (Physics.Raycast(transform.position + (Vector3.up * 0.1f), Vector3.down, out hitInfo, 0.1f))
+            if (Physics.Raycast(transform.position + (Vector3.up * 0.1f), Vector3.down, out hitInfo, 0.15f))
             {
                 // m_GroundNormal = hitInfo.normal;
                 isGrounded = true;
