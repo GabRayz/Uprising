78f946a7de17a3f5ac66ba7c1da6bd00716b10c5
Vardiak <contact@vardiak.fr>
2019-04-22 18:01:34 +0200

    Clean client side code

--- Web/client/src/js/index.js
+++ Web/client/src/js/index.js
@@ -6,37 +6,43 @@ document.body.onscroll = e => {
     header.style.backgroundColor = 'rgba(40, 40, 40, ' + range + ')';
 };
 
-let playButton = document.getElementsByClassName("playButton")[0];
+let playButton = document.querySelector('.playButton');
 
-playButton.addEventListener("mouseenter", function(e) {
-    document.getElementsByClassName("playButton_bg")[0].style.backgroundImage = "url(/assets/img/play_hover.png)";
-})
-playButton.addEventListener("mouseleave", function(e) {
-    document.getElementsByClassName("playButton_bg")[0].style.backgroundImage = "url(/assets/img/play.png)";
-})
-playButton.addEventListener("click", function(e) {
-    document.getElementsByClassName("playButton_bg")[0].style.backgroundImage = "url(/assets/img/play_clic.png)";
-})
+playButton.addEventListener('mouseenter', e => {
+    document.querySelector('.playButton_bg').style.backgroundImage =
+        'url(/assets/img/play_hover.png)';
+});
 
-let popup = document.getElementsByClassName("popup")[0];
+playButton.addEventListener('mouseleave', e => {
+    document.querySelector('.playButton_bg').style.backgroundImage = 'url(/assets/img/play.png)';
+});
 
-function displayLogin() {
-    console.log("display login");
-    popup.style.visibility = (popup.style.visibility == "visible") ? "hidden" : "visible";
-}
+playButton.addEventListener('click', e => {
+    document.querySelector('.playButton_bg').style.backgroundImage =
+        'url(/assets/img/play_clic.png)';
+});
+
+let popup = document.querySelector('.popup');
+
+document.querySelector('#navBar > li.nav_login > a').onclick = e => {
+    console.log('display login');
+    popup.style.visibility = popup.style.visibility == 'visible' ? 'hidden' : 'visible';
+};
 
 playButton.onclick = e => {
     while (document.body.firstChild) {
         document.body.removeChild(document.body.firstChild);
     }
 
-    const container = document.createElement("div");
-    container.id = "container";
-    container.style.width = "100%";
-    container.style.height = "100%";
+    const container = document.createElement('div');
+    container.id = 'container';
+    container.style.width = '100%';
+    container.style.height = '100%';
     document.body.appendChild(container);
 
-    const instance = UnityLoader.instantiate("container", "/assets/game/Build/game.json", { onProgress: UnityProgress });
+    const instance = UnityLoader.instantiate('container', '/assets/game/Build/game.json', {
+        onProgress: UnityProgress
+    });
 
     instance.SetFullscreen(1);
 };
