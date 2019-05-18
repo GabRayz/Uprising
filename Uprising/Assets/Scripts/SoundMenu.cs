32cce48e9f6f9deff6226466d6149c393a66fb95
Gabriel Rayzal <gabriel.rayzal@gmail.com>
2019-05-18 16:00:16 +0200

    Set sound in menu works

---
+++ Uprising/Assets/Scripts/SoundMenu.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+public class SoundMenu : MonoBehaviour
+{
+    public AudioMixer mixer;
+
+    public Slider master;
+    public Slider effect;
+    public Slider music;
+
+    private void Start()
+    {
+        float masterVolume;
+        mixer.GetFloat("MasterVolume", out masterVolume);
+        master.value = masterVolume;
+
+        float effectVolume;
+        mixer.GetFloat("EffectVolume", out effectVolume);
+        effect.value = effectVolume;
+
+        float musicVolume;
+        mixer.GetFloat("MusicVolume", out musicVolume);
+        music.value = musicVolume;
+    }
+
+    public void SetMasterVolume(float volume)
+    {
+        mixer.SetFloat("MasterVolume", volume);
+    }
+
+    public void SetEffectVolume(float volume)
+    {
+        mixer.SetFloat("EffectVolume", volume);
+    }
+
+    public void SetMusicVolume(float volume)
+    {
+        mixer.SetFloat("MusicVolume", volume);
+    }
+}
