7ece49a68efb4c69e44b685510b9ca1db35a16a8
Vardiak <contact@vardiak.fr>
2019-05-26 17:14:21 +0200

    Add additional stats to user model

--- Web/server/index.js
+++ Web/server/index.js
@@ -64,6 +64,22 @@ const User = db.define('user', {
     },
     password: {
         type: Sequelize.STRING
+    },
+    winCount: {
+        type: Sequelize.INTEGER,
+        defaultValue: 0
+    },
+    gameCount: {
+        type: Sequelize.INTEGER,
+        defaultValue: 0
+    },
+    shotCount: {
+        type: Sequelize.INTEGER,
+        defaultValue: 0
+    },
+    accurateShotCount: {
+        type: Sequelize.INTEGER,
+        defaultValue: 0
     }
 });
 
